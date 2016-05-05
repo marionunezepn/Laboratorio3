e004c5b4eb85bba43b77cb48b87463f815069a4d
oscarepv <oscar_e1993@hotmail.com>
2016-05-05 09:54:22 -0500

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -10,18 +10,6 @@ for vuelta in range (1,10):
 while(vuelta<10):
 	print("vuelta:"+str(vuelta))
 	vuelta=vuelta+1
-#con for
-print("con el for")
-for vuelta in range (1,10):
-	print("vuelta:"+str(vuelta))
-	
-	vuelta=1
-while(vuelta<10):
-	print("vuelta:"+str(vuelta))
-	vuelta=vuelta+1
-#con for
-print("con el for")
-for vuelta in range (1,10):
-	print("vuelta:"+str(vuelta))
+
 	
 	
