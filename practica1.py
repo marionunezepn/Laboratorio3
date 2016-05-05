453e40c18ff97c0ada8567a46c51f27020a76c65
Mario Jose Nuñez Villacis <pepeseya@hotmail.com>
2016-05-05 09:51:31 -0500

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -6,3 +6,22 @@ while(vuelta<10):
 print("con el for")
 for vuelta in range (1,10):
 	print("vuelta:"+str(vuelta))
+	vuelta=1
+while(vuelta<10):
+	print("vuelta:"+str(vuelta))
+	vuelta=vuelta+1
+#con for
+print("con el for")
+for vuelta in range (1,10):
+	print("vuelta:"+str(vuelta))
+	
+	vuelta=1
+while(vuelta<10):
+	print("vuelta:"+str(vuelta))
+	vuelta=vuelta+1
+#con for
+print("con el for")
+for vuelta in range (1,10):
+	print("vuelta:"+str(vuelta))
+	
+	
